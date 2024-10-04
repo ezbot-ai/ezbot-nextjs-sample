e28666c6291aadca376abe2883ae8a4353232faf
James Nowell <james@ezbot.ai>
2024-10-04 09:52:23 -0500

    Basic example of state and component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,48 @@
+"use strict";
+"use client";
+import {
+  initEzbot,
+  startActivityTracking,
+  trackPageView,
+  trackRewardEvent,
+  makeVisualChanges,
+} from "@ezbot-ai/javascript-sdk";
+import {createContext, useContext, useState, useEffect, useRef} from "react";
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+const defaultState = {
+  predictions: null,
+};
+
+const EzbotContext = createContext(defaultState)
+
 export default function Home() {
+  const ezbotInit = useRef(false);
+  const [currentEzbot, setCurrentEzbot] = useState(null);
+
+  useEffect(() => {
+    async function ezbotInitFn() {
+      if (ezbotInit.current) {
+        return;
+      }
+      try {
+        await initEzbot(38); // Replace 0 with your project ID
+        // startActivityTracking({
+        //   minimumVisitLength: 2,
+        //   heartbeatDelay: 2,
+        // });
+        // trackPageView();
+        makeVisualChanges();
+        ezbotInit.current = true;
+        setCurrentEzbot({predictions: window.ezbot.predictions})
+        console.log("Ezbot initialized successfully");
+      } catch (error) {
+        console.error("Error initializing Ezbot:", error);
+      }
+    }
+    ezbotInitFn();
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -10,122 +51,20 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <EzbotContext.Provider value={{currentEzbot, setCurrentEzbot}}>
+        <EzbotTestComponent></EzbotTestComponent>
+        </EzbotContext.Provider>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
+};
+
+export function EzbotTestComponent() {
+  const currentEzbot = useContext(EzbotContext);
+
+  if (currentEzbot !== null) {
+    return <p>Predictions: {currentEzbot.predictions}.</p>;
+  }
+
+  return <p>No Predictions.</p>;
 }
